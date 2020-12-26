9fd0a383c8a9030cdbfb4e1bace2561bfa60aa9f
sancaryum <sancaryum@gmail.com>
2020-12-26 22:34:36 +0300

    Last

---
+++ app/src/main/java/com/ossovita/navigation/FirstFragment.java
@@ -0,0 +1,53 @@
+package com.ossovita.navigation;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class FirstFragment extends Fragment {
+
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //NavDirections action = FirstFragmentDirections.actionFirstFragmentToSecondFragment();
+                FirstFragmentDirections.ActionFirstFragmentToSecondFragment action = FirstFragmentDirections.actionFirstFragmentToSecondFragment();
+                action.setAge(50);
+
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/ossovita/navigation/SecondFragment.java
@@ -0,0 +1,56 @@
+package com.ossovita.navigation;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SecondFragment extends Fragment {
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button button = view.findViewById(R.id.button2);
+        TextView textView = view.findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavDirections action = SecondFragmentDirections.actionSecondFragmentToFirstFragment();
+                Navigation.findNavController(view).navigate(action);
+            }
+        });
+        if(getArguments()!=null){
+            int age = SecondFragmentArgs.fromBundle(getArguments()).getAge();
+            textView.setText("Age: " + age );
+        }
+    }
+
+
+}
